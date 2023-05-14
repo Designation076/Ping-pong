2d76ab35ec98f2adab19262def4edf87c51808bc
Designation076 <132198879+Designation076@users.noreply.github.com>
2023-05-14 13:50:53 +0300

    Add files via upload
    minor update
    
    second player was replaced on simple bot

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
 from pygame import *
+
+rac_pic_r = 'p1.png'
+rac_pic_l = 'p2.png'
+ball_pic = 'Ball.png'
+
+
+
 class GameSprite(sprite.Sprite):
     #конструктор класса
        #конструктор класса
@@ -18,11 +25,11 @@ class GameSprite(sprite.Sprite):
         window.blit(self.image, (self.rect.x, self.rect.y))
 
 class Player(GameSprite):
-    def update_l(self):
-        keys = key.get_pressed()
-        if keys[K_UP] and self.rect.y > 5:
+    def update_l(self, turn):
+        #keys = key.get_pressed()
+        if turn == 'up' and self.rect.y > 5:
             self.rect.y -= self.speed
-        if keys[K_DOWN] and self.rect.y < win_height - 80:
+        if turn == 'down' and self.rect.y < win_height - 80:
             self.rect.y += self.speed
     def update_r(self):
         keys = key.get_pressed()
@@ -37,16 +44,16 @@ win_width = 500
 window = display.set_mode((win_width, win_height))
 window.fill(back)
 
-speed_x = 1
-speed_y = 1
+speed_x = 5
+speed_y = 5
 game = True
 finish = False
 clock = time.Clock()
 FPS = 60
 
-racket_r = Player("p1.png", 30, 200, 4, 25, 75)
-racket_l = Player("p2.png", 420, 200, 4, 25, 75)
-ball = GameSprite("Ball.png", 200, 200, 4, 50, 50)
+racket_r = Player(rac_pic_r, 30, 200, 10, 25, 75)
+racket_l = Player(rac_pic_l, 450, 200, 10, 25, 75)
+ball = GameSprite(ball_pic, 200, 200, 4, 50, 50)
 
 font.init()
 font = font.Font(None, 35)
@@ -61,7 +68,7 @@ while game:
     if not finish:
         window.fill(back)
         racket_r.update_r()
-        racket_l.update_l()
+        #racket_l.update_l()
 
         if ball.rect.y < 0 or ball.rect.y > 450:
             speed_y *= -1
@@ -76,6 +83,11 @@ while game:
         if ball.rect.x > win_width:
             finish = True
             window.blit(lose2, (200, 200))
+        
+        if racket_l.rect.y > ball.rect.y:
+            racket_l.update_l("up")
+        if racket_l.rect.y < ball.rect.y:
+            racket_l.update_l("down")
         
         ball.rect.x += speed_x
         ball.rect.y += speed_y
